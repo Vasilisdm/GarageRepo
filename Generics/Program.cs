4acac8fabe7487ddac126dfa20c743d8fdcd075b
vasilisdimitriou <vasilisdmr@gmail.com>
2020-03-16 18:44:26 +0200

    Initial commit

---
+++ Generics/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Generics
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            static string[] GenerateNames()
+            {
+                string[] names = new string[4]; names[0] = "Gamma";
+                names[1] = "Vlissides"; names[2] = "Johnson";
+                names[3] = "Helm";
+                return names;
+            }
+
+            static void PrintNames(string[] names)
+            {
+                foreach (string name in names)
+                {
+                    Console.WriteLine(name);
+                }
+            }
+        }
+    }
+}
