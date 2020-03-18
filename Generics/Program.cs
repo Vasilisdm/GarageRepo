5d8fd7baac298d4326314a86e17eef75d2de1260
vasilisdimitriou <vasilisdmr@gmail.com>
2020-03-18 21:33:31 +0200

    Implemented PrintType which writes in the console the typeof(T) and typeof(List<t>)

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -9,19 +9,22 @@ namespace Generics
     {
         public static void Main(string[] args)
         {
-            List<int> numbers = new List<int>();
+            //List<int> numbers = new List<int>();
 
-            numbers.Add(5);
-            numbers.Add(1);
-            numbers.Add(7);
+            //numbers.Add(5);
+            //numbers.Add(1);
+            //numbers.Add(7);
 
-            List<int> firstTwo = CopyAtMost(numbers, 2);
-            Console.WriteLine($"The first 2 elements of the numbers list copied, to FirstTwo list");
+            //List<int> firstTwo = CopyAtMost(numbers, 2);
+            //Console.WriteLine($"The first 2 elements of the numbers list copied, to FirstTwo list");
 
-            foreach (int number in firstTwo)
-            {
-                Console.WriteLine($"{number}");
-            }
+            //foreach (int number in firstTwo)
+            //{
+            //    Console.WriteLine($"{number}");
+            //}
+
+            PrintType<string>();
+            PrintType<int>();
         }
 
 
@@ -57,6 +60,13 @@ namespace Generics
             }
 
             return ret;
+        }
+
+
+        public static void PrintType<T>()
+        {
+            Console.WriteLine($"{typeof(T)}");
+            Console.WriteLine($"{typeof(List<T>)}");
         }
     }
 }
