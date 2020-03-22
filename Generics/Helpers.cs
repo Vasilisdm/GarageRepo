8c9c129046e7ee02aa7d061e5be06326950de7a8
vasilisdimitriou <vasilisdmr@gmail.com>
2020-03-22 16:35:54 +0200

    Moved the methods from within program class to their own Helpers class.

---
+++ Generics/Helpers.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Generics
+{
+    public class Helpers
+    {
+        public static void PrintItems<T>(List<T> items) where T : IFormattable
+        {
+            CultureInfo culture = CultureInfo.InvariantCulture;
+            foreach (T item in items)
+            {
+                Console.WriteLine(item.ToString(null, culture));
+            }
+        }
+
+        public static List<T> CopyAtMost<T>(List<T> input, int maxElements)
+        {
+            int actualCount = Math.Min(input.Count, maxElements);
+            List<T> ret = new List<T>(actualCount);
+            for (int i = 0; i < actualCount; i++)
+            {
+                ret.Add(input[i]);
+            }
+
+            return ret;
+        }
+
+
+        public static T LastOrDefault<T>(IEnumerable<T> source)
+        {
+            T ret = default(T);
+
+            foreach (T item in source)
+            {
+                ret = item;
+            }
+
+            return ret;
+        }
+
+
+        public static void PrintType<T>()
+        {
+            Console.WriteLine($"{typeof(T)}");
+            Console.WriteLine($"{typeof(List<T>)}");
+        }
+    }
+}
--- Generics/Program.cs
+++ Generics/Program.cs
@@ -26,47 +26,5 @@ namespace Generics
             PrintType<string>();
             PrintType<int>();
         }
-
-
-        public static void PrintItems<T>(List<T> items) where T : IFormattable
-        {
-            CultureInfo culture = CultureInfo.InvariantCulture;
-            foreach (T item in items)
-            {
-                Console.WriteLine(item.ToString(null, culture));
-            }
-        }
-
-        public static List<T> CopyAtMost<T>(List<T> input, int maxElements)
-        {
-            int actualCount = Math.Min(input.Count, maxElements);
-            List<T> ret = new List<T>(actualCount);
-            for (int i = 0; i < actualCount; i++)
-            {
-                ret.Add(input[i]);
-            }
-
-            return ret;
-        }
-
-
-        public static T LastOrDefault<T>(IEnumerable<T> source)
-        {
-            T ret = default(T);
-
-            foreach (T item in source)
-            {
-                ret = item;
-            }
-
-            return ret;
-        }
-
-
-        public static void PrintType<T>()
-        {
-            Console.WriteLine($"{typeof(T)}");
-            Console.WriteLine($"{typeof(List<T>)}");
-        }
     }
 }
