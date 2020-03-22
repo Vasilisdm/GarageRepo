40ae149426ced500fc622e742f5d3b6713d0966c
vasilisdimitriou <vasilisdmr@gmail.com>
2020-03-22 16:37:52 +0200

    Did a refactor in Main's method code to be more readable.

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -1,30 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.Specialized;
-using System.Globalization;
-
-namespace Generics
+﻿namespace Generics
 {
     class MainClass
     {
         public static void Main(string[] args)
         {
-            //List<int> numbers = new List<int>();
-
-            //numbers.Add(5);
-            //numbers.Add(1);
-            //numbers.Add(7);
-
-            //List<int> firstTwo = CopyAtMost(numbers, 2);
-            //Console.WriteLine($"The first 2 elements of the numbers list copied, to FirstTwo list");
-
-            //foreach (int number in firstTwo)
-            //{
-            //    Console.WriteLine($"{number}");
-            //}
-
-            PrintType<string>();
-            PrintType<int>();
+            Helpers.PrintType<string>();
+            Helpers.PrintType<int>();
         }
     }
 }
