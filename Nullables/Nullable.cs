b2a23c03085360c320c524e8c72855b89ef0c813
vasilisdimitriou <vasilisdmr@gmail.com>
2020-03-22 20:48:01 +0200

    Implemented a primative version of Nullable<T>

---
+++ Nullables/Nullable.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace Nullables
+{
+    public struct Nullable<T> where T : struct
+    {
+        private readonly T _value;
+        private readonly bool _hasValue;
+
+        public Nullable(T value)
+        {
+            _value = value;
+            _hasValue = true;
+        }
+
+        public bool HasValue { get { return _hasValue; } }
+
+        public T Value
+        {
+            get
+            {
+                if (!_hasValue)
+                {
+                    throw new InvalidOperationException();
+                }
+
+                return _value;
+            }
+        }
+    }
+}
