27b82c777a3a7d8ec3c956fc3cfbc43a38b65ce8
vasilisdimitriou <vasilisdmr@gmail.com>
2020-03-23 20:46:10 +0200

    Created 2 new instances of the nullable<T> in order to check the reference when it's null and not.

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -1,15 +1,18 @@
-﻿namespace Generics
+﻿using System;
+
+namespace Generics
 {
     class MainClass
     {
         public static void Main(string[] args)
         {
-            GenericCounter<string>.Increment();
-            GenericCounter<string>.Increment();
-            GenericCounter<string>.Display();
-            GenericCounter<int>.Display();
-            GenericCounter<int>.Increment();
-            GenericCounter<int>.Display();
+            int? noValue = new int?();
+            object noValueBoxed = noValue;
+            Console.WriteLine($"The boxed value is null: {noValueBoxed == null}");
+
+            int? someValue = new int?(5);
+            object someValueBoxed = someValue;
+            Console.WriteLine($"The boxed value is not null and has a type of :{someValueBoxed.GetType()}");
         }
     }
 }
